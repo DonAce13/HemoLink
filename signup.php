42d467b616868712f55815abc7f009a8865a2527
DonAce13 <154772513+DonAce13@users.noreply.github.com>
2024-12-09 07:33:47 +0800

    Clean initial commit

---
+++ signup.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/animations.css">  
+    <link rel="stylesheet" href="css/main.css">  
+    <link rel="stylesheet" href="css/signup.css">
+        
+    <title>Sign Up</title>
+    
+</head>
+<body>
+<?php
+session_start();
+
+// Unset all server-side variables
+$_SESSION["user"] = "";
+$_SESSION["usertype"] = "";
+
+// Set the timezone
+date_default_timezone_set('Asia/Kolkata');
+$date = date('Y-m-d');
+$_SESSION["date"] = $date;
+
+$error = ""; // Error message variable
+
+if ($_POST) {
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $address = $_POST['address'];
+    $nic = $_POST['nic'];
+    $dob = $_POST['dob'];
+    $email = $_POST['email'] ?? ""; // Email field (assume you have it in the form)
+    $newpassword = $_POST['password'] ?? ""; // Password field
+    $cpassword = $_POST['confirm_password'] ?? ""; // Confirm password field
+    $tele = $_POST['tele'] ?? ""; // Telephone field
+
+    // Server-side validation
+    if (!preg_match("/^[a-zA-Z]+$/", $fname) || !preg_match("/^[a-zA-Z]+$/", $lname)) {
+        $error = '<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;">First name and last name can only contain letters.</label>';
+    } elseif ($newpassword !== $cpassword) {
+        $error = '<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;">Password Confirmation Error! Reconfirm Password</label>';
+    } else {
+        // Database connection (replace with your database details)
+        $database = new mysqli("localhost", "root", "", "edoc");
+        if ($database->connect_error) {
+            die("Connection failed: " . $database->connect_error);
+        }
+
+        // Check if email already exists in the webuser table
+        $emailResult = $database->query("SELECT * FROM webuser WHERE email='$email';");
+        if ($emailResult->num_rows > 0) {
+            $error = '<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;">Already have an account for this Email address.</label>';
+        } else {
+            // Check if mobile number already exists in the patient table
+            $teleResult = $database->query("SELECT * FROM patient WHERE ptel='$tele';");
+            if ($teleResult->num_rows > 0) {
+                $error = '<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;">Mobile number is already registered.</label>';
+            } else {
+                // Insert new record into the patient table
+                $database->query("INSERT INTO patient (pemail, pname, ppassword, paddress, pnic, pdob, ptel) VALUES ('$email', '$fname $lname', '$newpassword', '$address', '$nic', '$dob', '$tele');");
+
+                // Insert new record into the webuser table
+                $database->query("INSERT INTO webuser (email, usertype) VALUES ('$email', 'p');");
+
+                $_SESSION["user"] = $email;
+                $_SESSION["usertype"] = "p";
+                $_SESSION["username"] = $fname;
+
+                header('Location: patient/index.php');
+                exit();
+            }
+        }
+
+        $database->close();
+    }
+}
+?>
+
+<center>
+<div class="container">
+    <table border="0">
+        <tr>
+            <td colspan="2">
+                <p class="header-text">Let's Get Started</p>
+                <p class="sub-text">Add Your Personal Details to Continue</p>
+                <?php if ($error) echo $error; ?>
+            </td>
+        </tr>
+        <tr>
+            <form action="" method="POST">
+            <td class="label-td" colspan="2">
+                <label for="name" class="form-label">Name: </label>
+            </td>
+        </tr>
+        <tr>
+            <td class="label-td">
+                <input type="text" name="fname" class="input-text" placeholder="First Name" required>
+            </td>
+            <td class="label-td">
+                <input type="text" name="lname" class="input-text" placeholder="Last Name" required>
+            </td>
+        </tr>
+        <tr>
+            <td class="label-td" colspan="2">
+                <label for="address" class="form-label">Address: </label>
+            </td>
+        </tr>
+        <tr>
+            <td class="label-td" colspan="2">
+                <input type="text" name="address" class="input-text" placeholder="Address" required>
+            </td>
+        </tr>
+        <tr>
+            <td class="label-td" colspan="2">
+                <label for="nic" class="form-label">NIC: </label>
+            </td>
+        </tr>
+        <tr>
+            <td class="label-td" colspan="2">
+                <input type="text" name="nic" class="input-text" placeholder="NIC Number" required>
+            </td>
+        </tr>
+        <tr>
+            <td class="label-td" colspan="2">
+                <label for="dob" class="form-label">Date of Birth: </label>
+        <?php
+        $today = date('Y-m-d');
+        echo '<input type="date" name="dob" class="input-text" max="' . $today . '" required>';
+        ?>
+        
+            </td>
+        </tr>
+
+        <!-- Add email, password, confirm password, and telephone fields -->
+        <tr>
+            <td class="label-td" colspan="2">
+                <label for="email" class="form-label">Email: </label>
+                <input type="email" name="email" class="input-text" placeholder="Email Address" required>
+            </td>
+        </tr>
+        <tr>
+            <td class="label-td">
+                <label for="password" class="form-label">Password: </label>
+                <input type="password" name="password" class="input-text" placeholder="Password" required>
+            </td>
+            <td class="label-td">
+                <label for="confirm_password" class="form-label">Confirm Password: </label>
+                <input type="password" name="confirm_password" class="input-text" placeholder="Confirm Password" required>
+            </td>
+        </tr>
+        <tr>
+            <td class="label-td" colspan="2">
+                <label for="tele" class="form-label">Telephone: </label>
+                <input type="tel" name="tele" class="input-text" placeholder="Telephone Number" required>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="reset" value="Reset" class="login-btn btn-primary-soft btn">
+            </td>
+            <td>
+                <input type="submit" value="Next" class="login-btn btn-primary btn">
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <br>
+                <label for="" class="sub-text" style="font-weight: 280;">Already have an account&#63; </label>
+                <a href="login.php" class="hover-link1 non-style-link">Login</a>
+                <br><br><br>
+            </td>
+        </tr>
+            </form>
+        </tr>
+    </table>
+</div>
+</center>
+</body>
+</html>
